c568d71db8d32eec3935e3be4484751040aa9c46
JuneCho23 <junecho.ucb@gmail.com>
2017-05-03 10:48:40 -0700

    Fin

--- SeattleTimes/Tests.cs
+++ SeattleTimes/Tests.cs
@@ -39,19 +39,19 @@ namespace SeattleTimes
 			Thread.Sleep(8000);
 			app.Tap("up");
 			app.Screenshot("Let's start by Tapping on the Hamburger Icon");
-			Thread.Sleep(4000);
+			Thread.Sleep(5000);
 			app.Tap("Top Stories");
 			app.Screenshot("Then we Tapped on 'Top Stories'");
 
 			app.Tap("up");
 			app.Screenshot("We Tapped on the Hamburger Icon");
-			Thread.Sleep(4000);
+			Thread.Sleep(5000);
 			app.Tap("Local News");
 			app.Screenshot("Next we Tapped 'Local News'");
 
 			app.ScrollDown();
 			app.Screenshot("We Scrolled Down for more information");
-			Thread.Sleep(4000);
+			Thread.Sleep(5000);
 
 			app.Tap("action_refresh");
 			app.Screenshot("Then we Tapped the 'Refresh' Button");
